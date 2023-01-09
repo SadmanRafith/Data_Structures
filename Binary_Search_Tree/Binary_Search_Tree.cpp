8729a059ad19af4214898675a63d00177e42e972
Sadman Rafith <108008599+SadmanRafith@users.noreply.github.com>
2023-01-09 15:56:44 +0600

    Update Binary_Search_Tree.cpp

--- Binary_Search_Tree/Binary_Search_Tree.cpp
+++ Binary_Search_Tree/Binary_Search_Tree.cpp
@@ -1,3 +1,22 @@
+/*
+Binary search tree (BST) is a data structure that allows fast search, insertion, and deletion of data. It is called 
+a binary search tree because each node has at most two children and the tree is ordered: for any given node, all values 
+in its left subtree are less than or equal to the value at the node, and all values in its right subtree are greater 
+than the value at the node.
+Here's how the BST algorithm works:
+1. Start at the root node.
+2. If the root node is null, return null.
+3. If the value you are searching for is less than the value at the root node, search the left subtree. 
+   If the value is greater than the value at the root node, search the right subtree.
+4. Repeat this process until you find the value you are searching for or until you reach a null node.
+5. Insertion and deletion in a BST follow a similar process. To insert a value, you find the appropriate 
+   place in the tree for it and add it as a new leaf node. To delete a value, you find the node with the 
+   value and either delete it (if it has no children), replace it with one of its children (if it has one child), 
+   or replace it with the smallest value in its right subtree (if it has two children).
+BSTs are efficient because the average time complexity for search, insert, and delete operations is O(log n), where n is the 
+number of nodes in the tree. This is much faster than a linear search, which has a time complexity of O(n).
+*/
+
 #include<iostream>
 using namespace std;
 
